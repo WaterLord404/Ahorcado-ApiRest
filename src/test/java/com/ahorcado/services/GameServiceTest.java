a2e3be44dcb1f58ab2bca7da09c055404d54d005
WaterLord404 <davidgomezrodriguezinfor@gmail.com>
2020-12-21 12:44:18 +0100

    cambio import junit

--- src/test/java/com/ahorcado/services/GameServiceTest.java
+++ src/test/java/com/ahorcado/services/GameServiceTest.java
@@ -1,5 +1,5 @@
 package com.ahorcado.services;
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
